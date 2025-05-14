18272373b63d48fccb1565251f4e512781530be8
nevo0309 <nevo19970309@gmail.com.com>
2025-05-14 18:04:01 +0300

    Add the option to have users in the app

---
+++ api/user/user.routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+import { requireAdmin, requireAuth } from '../../middleware/requireAuth.middleware.js'
+import { deleteUser, getUser, getUsers, updateUser } from './user.controller.js'
+
+export const userRoutes = express.Router()
+
+userRoutes.get('/', getUsers)
+userRoutes.get('/:userId', getUser)
+userRoutes.put('/:userId', updateUser)
+userRoutes.delete('/:userId', deleteUser)
+// userRoutes.delete('/:userId', requireAuth, requireAdmin, deleteUser)
